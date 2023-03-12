878e230f539de64de312d8ae0d80f2148d14c6f8
Daniella Omokore <daniellaomokore@gmail.com>
2023-03-12 18:07:30 +0000

    info modified

--- static/js/main.js
+++ static/js/main.js
@@ -64,127 +64,19 @@ window.onload = function() {
 }
 
 
+/* LAZY LOADING FOR IMAGES, VIDEOS, YOUTUBE VIDEOS, TWEETS AND INSTAGRAM POSTS */
 
 
-
-// JS --> This code selects all images, videos, YouTube videos, tweets, and Instagram posts on the page and sets their loading attribute to "lazy".
-
-
-// Set all images to have lazy loading
-const images = document.querySelectorAll("img");
-images.forEach((img) => {
-  img.setAttribute("loading", "lazy");
-});
-
-// Set all videos to have lazy loading
-const videos = document.querySelectorAll("video");
-videos.forEach((video) => {
-  video.setAttribute("loading", "lazy");
-});
-
-// Set all YouTube videos to have lazy loading
-const youtubeVideos = document.querySelectorAll("iframe[src*='youtube.com']");
-youtubeVideos.forEach((video) => {
-  video.setAttribute("loading", "lazy");
-});
-
-// Set all tweets to have lazy loading
-const tweets = document.querySelectorAll("blockquote.twitter-tweet");
-tweets.forEach((tweet) => {
-  tweet.setAttribute("loading", "lazy");
-});
-
-// Set all Instagram posts to have lazy loading
-const instaPosts = document.querySelectorAll("blockquote.instagram-media");
-instaPosts.forEach((post) => {
-  post.setAttribute("loading", "lazy");
+// Set lazy loading for images, videos, YouTube videos, tweets, and Instagram posts
+const elements = document.querySelectorAll('img, video, iframe[src*="youtube.com"], blockquote.twitter-tweet, blockquote.instagram-media');
+elements.forEach(element => {
+  element.setAttribute('loading', 'lazy');
 });
 
 
 
 
-
-
-
-// JS -> Lazy loading for images, videos, YouTube videos, tweets, and Instagram posts
-
-document.addEventListener("DOMContentLoaded", function() {
-  // Lazy load images
-  const lazyImages = document.querySelectorAll('img[loading="lazy"]');
-  lazyImages.forEach(img => {
-    if (img.complete) {
-      console.log("Image already loaded:", img);
-      return;
-    }
-    img.addEventListener("load", function() {
-      console.log("Image loaded:", img);
-    });
-    img.addEventListener("error", function() {
-      console.log("Error loading image:", img);
-    });
-    img.src = img.dataset.src;
-  });
-
-  // Lazy load videos
-  const lazyVideos = document.querySelectorAll('video[loading="lazy"]');
-  lazyVideos.forEach(video => {
-    if (video.complete) {
-      console.log("Video already loaded:", video);
-      return;
-    }
-    video.addEventListener("loadeddata", function() {
-      console.log("Video loaded:", video);
-    });
-    video.addEventListener("error", function() {
-      console.log("Error loading video:", video);
-    });
-    video.src = video.dataset.src;
-  });
-
-  // Lazy load YouTube videos
-  const lazyYouTubeVideos = document.querySelectorAll('iframe[data-src*="youtube"]');
-  lazyYouTubeVideos.forEach(ytVideo => {
-    const videoSrc = ytVideo.dataset.src;
-    ytVideo.addEventListener("load", function() {
-      console.log("YouTube video loaded:", ytVideo);
-    });
-    ytVideo.addEventListener("error", function() {
-      console.log("Error loading YouTube video:", ytVideo);
-    });
-    ytVideo.setAttribute("src", videoSrc);
-  });
-
-  // Lazy load tweets
-  const lazyTweets = document.querySelectorAll('blockquote.twitter-tweet[loading="lazy"]');
-  lazyTweets.forEach(tweet => {
-    const tweetSrc = tweet.dataset.src;
-    tweet.addEventListener("load", function() {
-      console.log("Tweet loaded:", tweet);
-    });
-    tweet.addEventListener("error", function() {
-      console.log("Error loading tweet:", tweet);
-    });
-    tweet.setAttribute("src", tweetSrc);
-  });
-
-  // Lazy load Instagram posts
-  const lazyInstagramPosts = document.querySelectorAll('blockquote.instagram-media[loading="lazy"]');
-  lazyInstagramPosts.forEach(post => {
-    const postSrc = post.dataset.src;
-    post.addEventListener("load", function() {
-      console.log("Instagram post loaded:", post);
-    });
-    post.addEventListener("error", function() {
-      console.log("Error loading Instagram post:", post);
-    });
-    post.setAttribute("src", postSrc);
-  });
-});
-
-
-/*This enables the dark and light mode*/
-
-/*
+/*THIS ENABLES THE DARK AND LIGHT MODE
 
 NOTE: I've wrapped the js in 'document.addEventListener('DOMContentLoaded', function()'
 
@@ -211,7 +103,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 
-// Contact form email validators
+// CONTACT FORM INPUT VALIDATORS
 
 // Get the form element
 const form = document.querySelector('form[name="contact"]');
